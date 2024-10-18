76eed583e4e4895b68b19c0207eda3aca2f0cce6
sachin <sachinbhatia2021@gmail.com>
2024-10-18 15:31:10 +0530

    rds

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 from flask import render_template, Flask, redirect, request, url_for
-import boto3
-import os
-from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 from dotenv import load_dotenv
-from boto3.dynamodb.conditions import Key
-from boto3.dynamodb.conditions import Key
+import mysql.connector.pooling
+from mysql.connector import Error
+import os
 
 # Load environment variables from .env file
 load_dotenv()
 
+Host = os.getenv("Host")
+user = os.getenv("User")
+db_password = os.getenv("db_password")
+db_name = os.getenv("db_name")
+
 app = Flask(__name__)
 
-# Initialize the DynamoDB client using environment variables
-dynamodb = boto3.resource(
-    'dynamodb',
-    aws_access_key_id=os.getenv('aws_access_key_id'),
-    aws_secret_access_key=os.getenv('aws_secret'),
-    region_name=os.getenv('region_name')
-)
+# Database connection pool for AWS database
+dbconfig = {
+    "host": Host,
+    "user": user,
+    "password": db_password,
+    "database": db_name
+}
+
+# connection Pool
+mydb_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool",
+                                                        pool_size=8,
+                                                        **dbconfig)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -41,54 +49,25 @@ def dash():
     return render_template('watt.html')
 
 @app.route('/data')
-def data_table():
-    table_name = 'Wattmeter' 
-    table = dynamodb.Table(table_name)
-
+def data_table():    
     try:
-        # Fetch data from DynamoDB
-        response = table.scan()
-        items = response['Items']
-
-        # Sort items by timestamp in descending order
-        sorted_items = sorted(items, key=lambda x: x.get('timestamp', 0), reverse=True)
-
+        
         # Render the data in your HTML template
-        return render_template('table.html', items=sorted_items)
-    
-    except (NoCredentialsError, PartialCredentialsError) as e:
-        print(f"Credentials error: {e}")
-        return "Could not access DynamoDB. Please check your AWS credentials.", 500
+        return render_template('table.html')
+
     except Exception as e:
-        print(f"Error fetching data from DynamoDB: {e}")
-        return "An error occurred while fetching data. Please try again later.", 500
-# 
+        return str(e), 500
 
 @app.route('/databyid')
 def databyid():
-    print()
-    table_name = 'Wattmeter' 
-    table = dynamodb.Table(table_name)
-
     try:
-        # Fetch data from DynamoDB where Device_id = id
-        response = table.scan(
-            FilterExpression=Key('Device_id').eq('1')  # Adjusted to your specific device_id
-        )
-        items = response['Items']
-
-        # Sort items by timestamp in descending order
-        sorted_items = sorted(items, key=lambda x: x.get('timestamp', 0), reverse=True)
         
-        # Render the data in your HTML template
         return render_template('watt.html', items=sorted_items)
     
-    except (NoCredentialsError, PartialCredentialsError) as e:
-        print(f"Credentials error: {e}")
-        return "Could not access DynamoDB. Please check your AWS credentials.", 500
     except Exception as e:
-        print(f"Error fetching data from DynamoDB: {e}")
+        print(f"Error: {e}")
         return "An error occurred while fetching data. Please try again later.", 500
+
 
 
 if __name__ == '__main__':
